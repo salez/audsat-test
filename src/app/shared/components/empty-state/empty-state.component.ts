aa781bf774de02ead7695555fa3115169149200e
salez <r.sales.s@gmail.com>
2023-05-11 16:35:35 -0300

    feat: add empty-state component

---
+++ src/app/shared/components/empty-state/empty-state.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-state',
+  templateUrl: './empty-state.component.html',
+  styleUrls: ['./empty-state.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true
+})
+export class EmptyStateComponent {
+
+}
