402e8ce3392eeb80b42712709e6f1154268ffe5d
salez <r.sales.s@gmail.com>
2023-05-11 12:02:07 -0300

    feat: update home page with general info

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageTitleComponent } from '@shared/components/page-title/page-title.component';
+import { PageContentComponent } from '@shared/components/page-content/page-content.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PageTitleComponent, PageContentComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
