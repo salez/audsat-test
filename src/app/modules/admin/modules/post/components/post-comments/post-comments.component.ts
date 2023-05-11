7c2e7ff96bd48d4ae8cdb7591e4b4236a31c751f
salez <r.sales.s@gmail.com>
2023-05-11 16:34:22 -0300

    feat: show post comments and user data in the side page

--- src/app/modules/admin/modules/post/components/post-comments/post-comments.component.ts
+++ src/app/modules/admin/modules/post/components/post-comments/post-comments.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, signal } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { PostComment } from '../../models/post-comment.model';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-post-comments',
@@ -7,5 +10,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostCommentsComponent {
+  @Input({required: true}) postId!: number;
 
+  protected comments$?: Observable<PostComment[]>;
+  protected loading = signal(true);
+
+  constructor(
+    private postService: PostService
+  ){}
+
+  ngOnInit(): void {
+    this.comments$ = this.postService.getCommentsByPost(this.postId).pipe(
+      tap(() => this.loading.set(false))
+    );
+  }
 }
--- src/app/modules/admin/modules/user/components/user-data/user-data.component.ts
+++ src/app/modules/admin/modules/user/components/user-data/user-data.component.ts
@@ -1,24 +1,32 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, signal } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { User } from '../../models/user.model';
 import { MatCardModule } from '@angular/material/card';
 import { CommonMaterialModule } from '@shared/common-material.module';
+import { LoadingShimmerComponent } from '@shared/components/loading-shimmer/loading-shimmer.component';
 
 @Component({
   selector: 'app-user-data',
   templateUrl: './user-data.component.html',
   styleUrls: ['./user-data.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, CommonMaterialModule, MatCardModule],
+  imports: [CommonModule, CommonMaterialModule, MatCardModule, LoadingShimmerComponent],
   standalone: true
 })
 export class UserDataComponent {
   @Input({ required: true }) set id(value: number) {
-    this.user$ = this.userService.getUser(value);
+    this.loading.set(true);
+    
+    this.user$ = this.userService.getUser(value).pipe(
+      tap(() => {
+        this.loading.set(false);
+      })
+    );
   }
 
+  protected loading = signal(true);
   protected user$: Observable<User> | undefined;
 
   constructor(
